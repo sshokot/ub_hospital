d971e2525ed179b3e619211f34da7e47e850011c
sshokot <30502008+sshokot@users.noreply.github.com>
2023-03-15 13:22:08 +0200

    Update main.py

--- controllers/main.py
+++ controllers/main.py
@@ -5,7 +5,6 @@ import json
 class MainHospital(http.Controller):
     @http.route('/hospital/new_visit', type='json', methods=['POST'], auth='none', csrf=False)
     def add_new_visit(self, **post):
-        print('params)',post)
-        print('http.request.params',http.request.params)
+           
         res = request.env['hospital.visit'].add_new_visit(post)
         return json.dumps(res)
